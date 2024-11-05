71663337c2667a2ff3a025f0ad2c025813d01b7e
giobauermeister <giovanni.bauermeister@gmail.com>
2024-11-05 19:41:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import LoadingScreen from './components/LoadingScreen.vue'
+
+// Ref to control loading screen visibility
+const showLoadingScreen = ref(true)
+
+onMounted(() => {
+  // Hide the loading screen after 2 seconds
+  setTimeout(() => {
+    showLoadingScreen.value = false
+  }, 2000)
+})
+</script>
+
+<template>
+  <!-- Conditionally render the loading screen or the main content -->
+  <LoadingScreen v-if="showLoadingScreen" />
+
+  <div v-else>
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,33 @@
+<!-- src/components/LoadingScreen.vue -->
+<template>
+    <div class="loading-screen">
+      <img src="../assets/logo_yellow.png" alt="Loading Logo" class="loading-logo" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LoadingScreen'
+  }
+  </script>
+  
+  <style scoped>
+  .loading-screen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #493147;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 1000; /* Ensure it's on top */
+  }
+  .loading-logo {
+    width: 150px;
+    margin-bottom: 0px;
+  }
+  </style>
+  
